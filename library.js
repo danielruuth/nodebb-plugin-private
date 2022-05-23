38ae14fb8c7ab928c46c073682b8058db25e172d
danielruuth <daniel.ruuth@gmail.com>
2022-05-23 13:47:23 +0200

    Added admin view

--- library.js
+++ library.js
@@ -3,24 +3,24 @@ let winston = module.parent.require('winston');
 const helpers = require.main.require('./src/controllers/helpers');
 
 plugin.privateforum = async (data, callback) => {
-
         var path = data.req.path;
 
+        //These will be set in an admin view later
         let privatePages = ['/categories','/recent','/tags','/popular','/users','/groups', '/unread'];
 
         winston.verbose(`Path is = ${path}, allowed is = ${privatePages.includes(path)}`);
 
-        if (req.loggedIn || privatePages.includes(path)) {
+        if (data.req.loggedIn || privatePages.includes(path)) {
                 winston.verbose("[plugin-nodebb-private-forum] User is logged or URL is allowed ("+ req.url +"), no redirect.");
         } else {
                 winston.verbose("[plugin-nodebb-private-forum] User is not logged and URL is not allowed ("+ data.req.url +"), redirecting to login page.");
                 helpers.notAllowed(data.req, data.res);
         }
 };
-/*plugin.init = async (params) => {
+plugin.init = async (params) => {
 	const { router, middleware, controllers } = params;
 
-	routeHelpers.setupPageRoute(router, '/private', middleware, [(req, res, next) => {
+	routeHelpers.setupPageRoute(router, '/private-plug', middleware, [(req, res, next) => {
 		setImmediate(next);
 	}], (req, res) => {
 		winston.info(`[plugins/private] Navigated to ${nconf.get('relative_path')}/private-plug`);
@@ -28,6 +28,6 @@ plugin.privateforum = async (data, callback) => {
 	});
 
 	routeHelpers.setupAdminPageRoute(router, '/admin/plugins/private', middleware, [], controllers.renderAdminPage);
-};*/
+};
 
 module.exports = plugin;
