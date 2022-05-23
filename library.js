c6c37cb97cea64cc7167ecf1fc88571f64ebca32
danielruuth <daniel.ruuth@gmail.com>
2022-05-23 13:09:44 +0200

    Change from allowed to private

--- library.js
+++ library.js
@@ -3,17 +3,31 @@ let winston = module.parent.require('winston');
 const helpers = require.main.require('./src/controllers/helpers');
 
 plugin.privateforum = async (data, callback) => {
-      
-        var req = data.req;
-        var res = data.res;
-        let allowedPages = /\/(assets\/|login|register|reset|auth|plugins\/).*|.*(.css|.js)$/;
 
-        if (req.loggedIn || allowedPages.test(req.url)) {
+        var path = req.path;
+
+        let privatePages = ['/categories','/recent','/tags','/popular','/users','/groups', '/unread'];
+
+        winston.verbose(`Path is = ${path}, allowed is = ${privatePages.includes(path)}`);
+
+        if (req.loggedIn || privatePages.includes(path)) {
                 winston.verbose("[plugin-nodebb-private-forum] User is logged or URL is allowed ("+ req.url +"), no redirect.");
         } else {
-                winston.verbose("[plugin-nodebb-private-forum] User is not logged and URL is not allowed ("+ req.url +"), redirecting to login page.");
-                helpers.notAllowed(req, res);
+                winston.verbose("[plugin-nodebb-private-forum] User is not logged and URL is not allowed ("+ data.req.url +"), redirecting to login page.");
+                helpers.notAllowed(data.req, data.res);
         }
+};
+plugin.init = async (params) => {
+	const { router, middleware/* , controllers */ } = params;
+
+	routeHelpers.setupPageRoute(router, '/private', middleware, [(req, res, next) => {
+		setImmediate(next);
+	}], (req, res) => {
+		winston.info(`[plugins/private] Navigated to ${nconf.get('relative_path')}/private`);
+		res.render('private', { uid: req.uid });
+	});
+
+	routeHelpers.setupAdminPageRoute(router, '/admin/plugins/private', middleware, [], controllers.renderAdminPage);
 };
 
 module.exports = plugin;
